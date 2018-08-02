99d768bc4efe455072fdbe63f11f0b25f3a71722
lhakseon <lee.hakseong@cern.ch>
2018-08-03 02:27:37 +0900

    delete try

--- EvR/sw_ntuple/sw_ntuple/SA_medians/swTool_SA_C.d
+++
@@ -1,4 +0,0 @@
-
-# DO NOT DELETE
-./swTool_SA_C.so: /usr/local/include/RVersion.h /usr/local/include/RConfig.h /usr/local/include/TClass.h /usr/local/include/TDictAttributeMap.h /usr/local/include/TInterpreter.h /usr/local/include/TROOT.h /usr/local/include/TBuffer.h /usr/local/include/TMemberInspector.h /usr/local/include/TError.h /usr/local/include/RtypesImp.h /usr/local/include/TIsAProxy.h /usr/local/include/TFileMergeInfo.h /usr/local/include/TCollectionProxyInfo.h /usr/local/bin/rootcling
-swTool_SA_C__ROOTBUILDVERSION= 6.10/02
--- EvR/sw_ntuple/sw_ntuple/roi_plots/roiTools_R1_C.d
+++
@@ -1,4 +0,0 @@
-
-# DO NOT DELETE
-./roiTools_R1_C.so: /Users/jhkim/root_6_10_06/include/RVersion.h /Users/jhkim/root_6_10_06/include/RConfig.h /Users/jhkim/root_6_10_06/include/TClass.h /Users/jhkim/root_6_10_06/include/TDictAttributeMap.h /Users/jhkim/root_6_10_06/include/TInterpreter.h /Users/jhkim/root_6_10_06/include/TROOT.h /Users/jhkim/root_6_10_06/include/TBuffer.h /Users/jhkim/root_6_10_06/include/TMemberInspector.h /Users/jhkim/root_6_10_06/include/TError.h /Users/jhkim/root_6_10_06/include/RtypesImp.h /Users/jhkim/root_6_10_06/include/TIsAProxy.h /Users/jhkim/root_6_10_06/include/TFileMergeInfo.h /Users/jhkim/root_6_10_06/include/TCollectionProxyInfo.h /usr/local/bin/rootcling
-roiTools_R1_C__ROOTBUILDVERSION= 6.10/06
